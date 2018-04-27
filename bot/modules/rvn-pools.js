d9b8d29e328123a96598657b8c3b21c923245c11
MSFTserver <sawyerlemmo@gmail.com>
2018-04-27 09:37:03 -0700

    update pools to use db helpers

--- bot/modules/rvn-pools.js
+++ bot/modules/rvn-pools.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
-let moment = require('moment-timezone');
-let poolsSchema = require('..\\' + '\\db-models\\pool.js');
+let findEntry = require('../db-helpers.js').findEntry;
+let newEntry = require('../db-helpers.js').newEntry;
+let updateEntry = require('../db-helpers.js').updateEntry;
 let hasRvnPoolsChannels = require('../helpers.js').hasRvnPoolsChannels;
 let isPoolOwner = require('../helpers.js').isPoolOwner;
 let inPrivate = require('../helpers.js').inPrivate;
 let config = require('config');
 let channelID = config.get('General').Channels.botspam;
-let logChannel = config.get('moderation').logchannel;
-let pm2Name = config.get('General').pm2Name;
 
 exports.commands = ['pools'];
 
@@ -37,12 +35,12 @@ exports.pools = {
     }
 
     function poolsList(bot, msg, suffix) {
-      var pools = mongoose.model('pools');
       if (!suffix) {
         msg.channel
           .send('Sending Pools list via DM')
           .then(message => message.delete(8000));
-        pools.find({}, function(err, docs) {
+        findEntry(bot, msg, 'pools', false, false, findPools);
+        function findPools(bot, msg, docs) {
           docs.forEach(function(results) {
             var OwnerID = results.poolOwnerID;
             var Owner = results.poolOwner;
@@ -90,7 +88,7 @@ exports.pools = {
               }
             });
           });
-        });
+        }
       } else {
         if (!hasRvnPoolsChannels(msg)) {
           msg.channel.send(
@@ -99,7 +97,8 @@ exports.pools = {
           return;
         }
         var regex = new RegExp(['^', suffix, '$'].join(''), 'i');
-        pools.find({ poolName: regex }, function(err, docs) {
+        findEntry(bot, msg, 'pools', 'poolName', regex, findPool);
+        function findPool(bot, msg, docs) {
           if (!docs || !docs[0]) {
             return;
           } else {
@@ -149,7 +148,7 @@ exports.pools = {
               }
             });
           }
-        });
+        }
       }
     }
 
@@ -164,153 +163,97 @@ exports.pools = {
         .filter(function(n) {
           return n !== '';
         });
-      var pools = mongoose.model('pools');
-      pools.count({ poolOwnerID: msg.author.id }, function(err, count) {
-        pools.find({ poolOwnerID: msg.author.id }, function(err, docs) {
-          if (!docs || !docs[0]) {
-            if (!words[1] || !words[2] || !words[3] || !words[4] || !words[5]) {
-              msg.channel.send(
-                'provide all required info for pool seprated by commas! extra info is not required, if providing extra info please do not include commas in the info message.\n!pools set, <poolName>, <poolURL>, <poolFee>, <poolStratum>, <poolPort>, <poolExtraInfo>\nExample: `!pools set Mining Panda, https://miningpanda.site, 1%, stratum+tcp://miningpanda.site:3636, 3636, diff starts at 10`\nif you would like to update your pools options and not chang others use `d` in place of info to leave default from database\nExample`!pools set, d, d, 0%, d, d, d`'
-              );
-              return;
-            }
-            if (words[2].includes('stratum+tcp')) {
-              msg.channel.send(
-                'poolURL is the url to the site for access from the web, not the stratum URL'
-              );
-              return;
-            }
-            var Name = words[1];
-            var SiteURL = words[2];
-            var Fee = words[3];
-            var Stratum = words[4];
-            var Port = words[5];
-            var Info = words[6] ? words[6] : 'no extra info provided';
-            var newPool = {
-              poolOwnerID: msg.author.id,
-              poolOwner: msg.author.username,
-              poolName: Name.trim(),
-              poolURL: SiteURL.trim(),
-              poolFee: Fee.trim(),
-              poolStratum: Stratum.trim(),
-              poolPort: Port.trim(),
-              poolInfo: Info.trim()
-            };
-            var newPool = new pools(newPool);
-            newPool
-              .save()
-              .then(newPool => {
-                bot.channels
-                  .get(logChannel)
-                  .send('Saved New pool: ' + msg.author.username);
-                msg.channel.send('Pool info Saved');
-              })
-              .catch(err => {
-                var time = moment()
-                  .tz('America/Los_Angeles')
-                  .format('MM-DD-YYYY hh:mm a');
-                bot.channels
-                  .get(logChannel)
-                  .send(
-                    '[' +
-                      time +
-                      ' PST][' +
-                      pm2Name +
-                      '] ERROR saving Pool:\n' +
-                      err
-                  );
-                console.log(
-                  '[' +
-                    time +
-                    ' PST][' +
-                    pm2Name +
-                    '] ERROR saving Pool:\n' +
-                    err
-                );
-              });
-          } else {
-            if (docs[0].poolOwnerID != msg.author.id) {
-              msg.channel.send(
-                'only the entry creator can edit this pools info!'
-              );
-              return;
-            }
-            var OwnerID = docs[0].poolOwnerID;
-            var Owner = docs[0].poolOwner;
-            if (words[1].trim() == 'd') {
-              var Name = docs[0].poolName;
-            } else {
-              var Name = words[1];
-            }
-            if (words[2].trim() == 'd') {
-              var SiteURL = docs[0].poolURL;
-            } else {
-              var SiteURL = words[2];
-            }
-            if (words[3].trim() == 'd') {
-              var Fee = docs[0].poolFee;
-            } else {
-              var Fee = words[3];
-            }
-            if (words[4].trim() == 'd') {
-              var Stratum = docs[0].poolStratum;
-            } else {
-              var Stratum = words[4];
-            }
-            if (words[5].trim() == 'd') {
-              var Port = docs[0].poolPort;
-            } else {
-              var Port = words[5];
-            }
-            if (words[6].trim() == 'd') {
-              var Info = docs[0].poolInfo;
-            } else {
-              var Info = words[6];
-            }
-            var newPool = {
-              poolOwnerID: OwnerID,
-              poolOwner: Owner,
-              poolName: Name.trim(),
-              poolURL: SiteURL.trim(),
-              poolFee: Fee.trim(),
-              poolStratum: Stratum.trim(),
-              poolPort: Port.trim(),
-              poolInfo: Info.trim()
-            };
-            pools
-              .updateOne({ poolOwnerID: msg.author.id }, { $set: newPool })
-              .then(pools => {
-                bot.channels
-                  .get(logChannel)
-                  .send('Updated pool: ' + msg.author.username);
-                msg.channel.send('Pool info Updated');
-              })
-              .catch(err => {
-                var time = moment()
-                  .tz('America/Los_Angeles')
-                  .format('MM-DD-YYYY hh:mm a');
-                bot.channels
-                  .get(logChannel)
-                  .send(
-                    '[' +
-                      time +
-                      ' PST][' +
-                      pm2Name +
-                      '] ERROR Updating Pool:\n' +
-                      err
-                  );
-                console.log(
-                  '[' +
-                    time +
-                    ' PST][' +
-                    pm2Name +
-                    '] ERROR Updating Pool:\n' +
-                    err
-                );
-              });
+      findEntry(bot, msg, 'pools', 'poolOwnerID', msg.author.id, getPool);
+      function getPool(bot, msg, docs) {
+        if (!docs || !docs[0]) {
+          if (!words[1] || !words[2] || !words[3] || !words[4] || !words[5]) {
+            msg.channel.send(
+              'provide all required info for pool seprated by commas! extra info is not required, if providing extra info please do not include commas in the info message.\n!pools set, <poolName>, <poolURL>, <poolFee>, <poolStratum>, <poolPort>, <poolExtraInfo>\nExample: `!pools set Mining Panda, https://miningpanda.site, 1%, stratum+tcp://miningpanda.site:3636, 3636, diff starts at 10`\nif you would like to update your pools options and not chang others use `d` in place of info to leave default from database\nExample`!pools set, d, d, 0%, d, d, d`'
+            );
+            return;
           }
-        });
-      });
+          if (words[2].includes('stratum+tcp')) {
+            msg.channel.send(
+              'poolURL is the url to the site for access from the web, not the stratum URL'
+            );
+            return;
+          }
+          var Name = words[1];
+          var SiteURL = words[2];
+          var Fee = words[3];
+          var Stratum = words[4];
+          var Port = words[5];
+          var Info = words[6] ? words[6] : 'no extra info provided';
+          var newPool = {
+            poolOwnerID: msg.author.id,
+            poolOwner: msg.author.username,
+            poolName: Name.trim(),
+            poolURL: SiteURL.trim(),
+            poolFee: Fee.trim(),
+            poolStratum: Stratum.trim(),
+            poolPort: Port.trim(),
+            poolInfo: Info.trim()
+          };
+          newEntry(bot, msg, 'pools', newPool);
+        } else {
+          if (docs[0].poolOwnerID != msg.author.id) {
+            msg.channel.send(
+              'only the entry creator can edit this pools info!'
+            );
+            return;
+          }
+          var OwnerID = docs[0].poolOwnerID;
+          var Owner = docs[0].poolOwner;
+          if (words[1].trim() == 'd') {
+            var Name = docs[0].poolName;
+          } else {
+            var Name = words[1];
+          }
+          if (words[2].trim() == 'd') {
+            var SiteURL = docs[0].poolURL;
+          } else {
+            var SiteURL = words[2];
+          }
+          if (words[3].trim() == 'd') {
+            var Fee = docs[0].poolFee;
+          } else {
+            var Fee = words[3];
+          }
+          if (words[4].trim() == 'd') {
+            var Stratum = docs[0].poolStratum;
+          } else {
+            var Stratum = words[4];
+          }
+          if (words[5].trim() == 'd') {
+            var Port = docs[0].poolPort;
+          } else {
+            var Port = words[5];
+          }
+          if (words[6].trim() == 'd') {
+            var Info = docs[0].poolInfo;
+          } else {
+            var Info = words[6];
+          }
+          var updatePool = {
+            poolOwnerID: OwnerID,
+            poolOwner: Owner,
+            poolName: Name.trim(),
+            poolURL: SiteURL.trim(),
+            poolFee: Fee.trim(),
+            poolStratum: Stratum.trim(),
+            poolPort: Port.trim(),
+            poolInfo: Info.trim()
+          };
+          updateEntry(
+            bot,
+            msg,
+            'pools',
+            'poolOwnerID',
+            msg.author.id,
+            updatePool
+          );
+        }
+      }
     }
   }
 };

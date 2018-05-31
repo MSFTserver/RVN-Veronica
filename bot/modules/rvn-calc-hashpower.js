88932b01406c91fa56107b97c5fce5b9b3db08e4
MSFTserver <sawyerlemmo@gmail.com>
2018-05-31 08:51:00 -0700

    add console to check issue

--- bot/modules/rvn-calc-hashpower.js
+++ bot/modules/rvn-calc-hashpower.js
@@ -38,12 +38,13 @@ exports.hashpower = {
     if (words[0] == 'power') {
       var offset = 1;
     }
+    console.log(offset);
     var myhashrate = getValidatedAmount(words[0 + offset]);
     if (!myhashrate) {
       msg.channel.send('Please Specify a hashrate in Mh/s!');
       return;
     }
-    var checkFiat = words[1 + offset].toUpperCase();
+    var checkFiat = words[1 + offset];
     console.log(checkFiat);
     if (!checkFiat) {
       var fiat = 'USD';

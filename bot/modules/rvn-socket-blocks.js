a840dc9699b7c7452ae0a03fe133b00960faa1df
MSFTserver <sawyerlemmo@gmail.com>
2018-06-06 22:39:36 -0700

    add socket to get block winners

---
+++ bot/modules/rvn-socket-blocks.js
@@ -0,0 +1,29 @@
+let moment = require('moment-timezone');
+let config = require('config');
+let BlocksWonChannel = config.get('SocketBots').BlocksWonChannel;
+let SocketUrl = config.get('SocketBots').SocketUrl;
+let app = require('http').createServer(handler)
+let socket = require('socket.io').listen(app)
+
+exports.custom = [
+    'socketBlocks'
+]
+
+exports.socketBlocks = function(bot) {
+  eventToListenTo = 'raven/block'
+  room = 'raven'
+  var socket = io(SocketUrl);
+  socket.on('connect', function() {
+    socket.emit('subscribe', room);
+  })
+  socket.on(eventToListenTo, function(data) {
+    console.log(data);
+    var poolName = data.block.poolInfo.poolName;
+    var poolUrl = data.block.poolInfo.url;
+    var blockHeight = data.block.height;
+    var blockHash = data.block.hash;
+    bot.channels
+      .get(BlocksWonChannel)
+      .send('Block ' + blockHeight + ' Won by [' + poolName + '](' + poolUrl +') [View Block](' + SocketUrl + '/block/' + blockHash + ')');
+}
+}

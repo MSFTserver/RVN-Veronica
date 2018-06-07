36a3ee47aff41156d2a73c2de9ae947ec2587326
MSFTserver <sawyerlemmo@gmail.com>
2018-06-07 11:30:56 -0700

    intial socket txs test

---
+++ bot/modules/rvn-socket-txs.js
@@ -0,0 +1,44 @@
+let moment = require('moment-timezone');
+let config = require('config');
+let BlocksWonChannel = config.get('SocketBots').BlocksWonChannel;
+let SocketUrl = config.get('SocketBots').SocketUrl;
+let socketClient = require('socket.io-client');
+
+exports.custom = ['socketBlocks'];
+
+exports.socketBlocks = function(bot) {
+  let eventToListenTo = 'raven/tx';
+  let room = 'raven';
+  let socket = socketClient(SocketUrl);
+  socket.on('connect', function() {
+    socket.emit('subscribe', room);
+  });
+  socket.on(eventToListenTo, function(data) {
+    console.log(data);
+    let dt = new Date();
+    let timestamp = moment()
+      .tz('America/Los_Angeles')
+      .format('MM-DD-YYYY hh:mm::ss a');
+      const embed = {
+        description:
+          'Won by [' +
+          poolName +
+          '](' +
+          poolUrl +
+          ')\n[View Block](' +
+          SocketUrl +
+          '/block/' +
+          blockHash +
+          ')',
+        color: 7976557,
+        footer: {
+          text: 'Last Updated | ' + timestamp + ' PST'
+        },
+        author: {
+          name: 'Block ' + blockHeight,
+          icon_url: 'https://i.imgur.com/nKHVQgq.png'
+        }
+      };
+      //bot.channels.get(BlocksWonChannel).send({ embed });
+  });
+};

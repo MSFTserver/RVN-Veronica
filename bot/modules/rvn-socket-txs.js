1d93231192c5c57d0e15ba9750e7c72448d6b7f4
MSFTserver <sawyerlemmo@gmail.com>
2018-06-07 14:22:03 -0700

    pretty print json

--- bot/modules/rvn-socket-txs.js
+++ bot/modules/rvn-socket-txs.js
@@ -27,7 +27,7 @@ exports.socketBlocks = function(bot) {
         vinAddy['address'] = vin[i].address
         vinAddresses.push(vinAddy);
       }
-      console.log('vin: ' + JSON.stringify(countDuplicates(vinAddresses)));
+      console.log('vin: ' + JSON.stringify(countDuplicates(vinAddresses), null, 2));
       for (l=0; l < vout.length; l++) {
         voutAddy = new Object();
         voutAddy['address'] = vout[l].address
@@ -42,9 +42,9 @@ exports.socketBlocks = function(bot) {
           voutObject['amount'] = voutAddresses[m].amount
           newVoutAddresses.push(voutObject);
         }
-        console.log('vout: ' + JSON.stringify(newVoutAddresses) +'\n'+ (voutAddresses.length - 4) + ' More');
+        console.log('vout: ' + JSON.stringify(newVoutAddresses, null, 2) +'\n'+ (voutAddresses.length - 4) + ' More');
       } else {
-        console.log('vout: ' + JSON.stringify(voutAddresses));
+        console.log('vout: ' + JSON.stringify(voutAddresses, null, 2));
       }
     }
     let dt = new Date();

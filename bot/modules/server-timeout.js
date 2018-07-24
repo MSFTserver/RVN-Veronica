eeaac92fd4e5fcbda7e3349e61cb8eefdb838e3d
MSFTserver <sawyerlemmo@gmail.com>
2018-07-24 14:00:44 -0700

    add timeout command
    fixed major fuck up

--- bot/modules/server-timeout.js
+++ bot/modules/server-timeout.js
@@ -24,29 +24,19 @@ exports.timeout = {
       );
       return;
     }
-<<<<<<< HEAD
     var suffix = msg.content.substring(
       9
     );
     var member = msg.mentions.members.first();
     var words = suffix
-=======
-    let member = msg.mentions.members.first();
-    let words = suffix
->>>>>>> 878b63a... add timeout command
       .trim()
       .split(' ')
       .filter(function(n) {
         return n !== '';
       });
-<<<<<<< HEAD
     var timer = getValidatedAmount(words[1]);
     var reason = words.slice(2);
     var timestamp = moment()
-=======
-    let timer = Number(words[1]);
-    let reason = words.slice(2);
->>>>>>> 878b63a... add timeout command
     if (member == '<@undefinded>' || member == undefined) {
       msg.reply(' The member you inserted to timeout was invalid!');
       return;
@@ -55,7 +45,6 @@ exports.timeout = {
       msg.reply(' Add a reason to Timeout ' + member + ' please.');
       return;
     }
-<<<<<<< HEAD
     if (timer === null)  {
       msg.reply(' Invalid Number, Add a timeframe in Minutes to Timeout ' + member + ' for');
       return;
@@ -138,80 +127,6 @@ exports.timeout = {
               );
           } else {
           msg.channel.send('user has already been put in timeout for ' + gotProfile[0].timer + ' minutes on ' + moment(gotProfile[0].time).tz('America/Los_Angeles').format('MM-DD-YYYY hh:mm a') + ' PST');
-=======
-    if (getValidatedAmount(timer) === null)  {
-      msg.reply(' Invalid Number, Add a timeframe in Minutes to Timeout ' + member + ' for');
-      return;
-    }
-      findEntry(bot, msg, 'users', 'accUserID', msg.author.id, findProfile);
-      function findProfile(bot, msg, gotProfile) {
-        var timestamp = moment()
-          .tz('America/Los_Angeles')
-          .format('MM-DD-YYYY hh:mm a');
-        if (!gotProfile) {
-          var TimeoutUser = {
-            userID: msg.author.id,
-            username: msg.author.username,
-            reason: reason,
-            time: timestamp,
-            timer: timer,
-            times: 0,
-            active: true
-          };
-          member.addRole(msg.guild.roles.find('name', 'Timeout'))
-            .then(
-              newEntry(bot, msg, 'timeout', TimeoutUser);
-              msg.channel.send(member + ' ** Has Been Put in Timeout for ' + gotProfile[0].timer + ' Minutes\nreason: ' + gotProfile[0].reason);
-              bot.channels
-                .get(modLogChannel)
-                .send(
-                  '[' +
-                    time +
-                    ' PST][' +
-                    pm2Name +
-                    '] ' +
-                    msg.author.username +
-                    'Put' + member + ' ** in Timeout for ' + gotProfile[0].timer + ' Minutes\nreason: ' + gotProfile[0].reason
-                );
-            );
-        } else {
-          if (!gotProfile[0].active) {
-            var TimeoutUser = {
-              username: msg.author.username,
-              reason: reason,
-              time: timestamp,
-              timer: timer,
-              times: Number(gotProfile[0].times) + 1,
-              active: true
-            };
-            member.addRole(msg.guild.roles.find('name', 'Timeout')).then(
-            updateEntry(bot, msg, 'timeout', 'userID', msg.author.id, TimeoutUser);
-            msg.channel.send(
-              member +
-              ' ** Has Been Put in Timeout a total of ' +
-              gotProfile[0].times +
-              ' times, this time for ' +
-              gotProfile[0].timer +
-              ' Minutes\nreason: ' +
-              gotProfile[0].reason);
-            bot.channels
-              .get(modLogChannel)
-              .send(
-                '[' + time + ' PST][' + pm2Name + '] ' +
-                msg.author.username +
-                'Put' +
-                member +
-                ' ** in Timeout total of ' +
-                gotProfile[0].times +
-                ' times, this time for ' +
-                gotProfile[0].timer +
-                ' Minutes\nreason: ' +
-                gotProfile[0].reason
-              );
-            );
-          } else {
-          msg.channel.send('user has already been put in timeout for ' + gotProfile[0].timer + ' minutes @ ' + gotProfile[0].time);
->>>>>>> 878b63a... add timeout command
           return;
         }
 
@@ -233,7 +148,6 @@ exports.timeoutChecker = function(bot) {
     var msg = null;
     findEntry(bot, msg, 'timeout', false, false, findUsers);
     function findUsers(bot, msg, docs) {
-<<<<<<< HEAD
       if (docs) {
       docs.forEach(function(results) {
         if (results.active) {
@@ -257,29 +171,5 @@ exports.timeoutChecker = function(bot) {
       });
     }
     }
-=======
-      docs.forEach(function(results) {
-        var user = results.username;
-        var userID = results.userID;
-        var member = bot.guild.members.find('id', userID)
-        var timeoutStart = results.time;
-        var timeoutFor = results.timer;
-        var timeoutDate = timeoutStart.add(timeoutFor, 'minutes');
-        var now = moment().tz('America/Los_Angeles')
-          .format('MM-DD-YYYY hh:mm a');
-        var then = moment(timeoutDate);
-        if (now > then) {
-          member.removeRole(bot.guild.roles.find('name', 'Timeout'))
-          .then(
-            var TimeoutUser = {
-              active: false
-            };
-            updateEntry(bot, msg, 'timeout', 'userID', results.userID, TimeoutUser);
-          )
-        }
-      });
-    }
-
->>>>>>> 878b63a... add timeout command
   }, 60 * 1000);
 }

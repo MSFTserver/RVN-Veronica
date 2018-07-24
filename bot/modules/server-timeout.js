affb59631ac2c581e546720fd644c6d5151635b1
MSFTserver <sawyerlemmo@gmail.com>
2018-07-24 11:52:10 -0700

    check

--- bot/modules/server-timeout.js
+++ bot/modules/server-timeout.js
@@ -184,7 +184,7 @@ exports.timeoutChecker = function(bot) {
         var user = results.username;
         var userID = results.userID;
         var member = bot.users.find('id', userID);
-        console.log(bot.users.find('id', userID))
+        console.log(bot)
         var timeoutStart = moment(results.time)
         var timeoutFor = results.timer;
         console.log(timeoutStart);

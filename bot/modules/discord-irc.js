a5f13ca8f8fbb7bb8d817b539bce0fa32a5fbfff
MSFTserver <sawyerlemmo@gmail.com>
2018-11-10 09:19:27 -0800

    disable irc link

--- bot/modules/discord-irc.js
+++ bot/modules/discord-irc.js
@@ -1,8 +1,8 @@
-const discordIRC = require('discord-irc').default;
-const config = require('config');
-const ircconfig = config.get('irc');
-exports.custom = ['irc'];
-
-exports.irc = function(bot) {
-  discordIRC([ircconfig]);
-};
+// const discordIRC = require('discord-irc').default;
+// const config = require('config');
+// const ircconfig = config.get('irc');
+// exports.custom = ['irc'];
+//
+// exports.irc = function(bot) {
+//   discordIRC([ircconfig]);
+// };

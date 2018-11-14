816c430eac5f7bcf60bc5e8ab32645b343cff03b
MSFTserver <sawyerlemmo@gmail.com>
2018-11-14 09:38:26 -0800

    update list number

--- bot/modules/asset-OTC.js
+++ bot/modules/asset-OTC.js
@@ -85,9 +85,10 @@ exports.asset = {
           groups.forEach(function(results) {
             var message = results.toString().replace(/,/g, '\n     ');
             var description = message + '\n    use `!asset list <assetName>` to see info about specific asset.';
+            var page = 0++
             msg.author.send({
               embed: {
-                title: '__**Assets listed for sale**__',
+                title: '__**Assets listed for sale**__ (page ' + page + ')',
                 description: '    ' + description,
                 color: 8995497
               }

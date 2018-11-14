d3fe17115355144552cefb7c8e9d9c49630c5db0
MSFTserver <sawyerlemmo@gmail.com>
2018-11-13 21:15:44 -0800

    fix help

--- bot/modules/asset-OTC.js
+++ bot/modules/asset-OTC.js
@@ -8,7 +8,7 @@ let channelID = config.get('General').Channels.botspam;
 exports.commands = ['OTC'];
 
 exports.OTC = {
-  usage: 'list',
+  usage: '**list**',
   description:
     'Displays all raven assets forsale OTC\n     (optionally add asset name to return just that assets info)\n**!OTC sell**, <assetName>, <!AdminAsset>, <assetType>, <assetUnits>, <assetQuantity>, <assetReissuable>, <assetIPFS>, <Price>\n     provide all required info for asset seprated by commas!\n     Example: `!OTC sell, VERONICA, Yes/No/True/False, Main/sub/unique, 0-8, 0, Yes/No/True/False, Yes/No/True/False/IPFShash, 10000 RVN`\n**!OTC update**, <assetName>, <!AdminAsset>, <assetType>, <assetUnits>, <assetQuantity>, <assetReissuable>, <assetIPFS>, <Price>\n     if you would like to update your assets info and not change other things use `d` in place of info to leave default from database\n     Example`!OTC update, d, d, d, d, d, True, d, d`',
   process: function(bot, msg, suffix) {
